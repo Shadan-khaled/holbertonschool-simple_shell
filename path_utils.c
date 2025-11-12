4a120c5f1adcbd0b83aa921af85cbf8dde8492c2
Own Algarni <s.xooxs@hotmail.com>
2025-11-12 17:59:57 +0000

    Add path_utils.c for PATH environment variable resolution

---
+++ path_utils.c
@@ -0,0 +1,80 @@
+#include "shell.h"
+
+/**
+ * get_env_var - gets environment variable value
+ * @name: variable name
+ *
+ * Return: value or NULL
+ */
+char *get_env_var(char *name)
+{
+	int i, j, len;
+
+	if (!name || !environ)
+		return (NULL);
+
+	len = strlen(name);
+
+	for (i = 0; environ[i]; i++)
+	{
+		for (j = 0; j < len && environ[i][j] == name[j]; j++)
+			;
+		if (j == len && environ[i][j] == '=')
+			return (&environ[i][j + 1]);
+	}
+	return (NULL);
+}
+
+/**
+ * find_in_path - searches for command in PATH
+ * @cmd: command name
+ *
+ * Return: full path or NULL
+ */
+char *find_in_path(char *cmd)
+{
+	char *path, *path_copy, *dir, *full_path;
+	struct stat st;
+	int cmd_len, dir_len;
+
+	if (!cmd)
+		return (NULL);
+
+	if (cmd[0] == '/' || cmd[0] == '.')
+		return (stat(cmd, &st) == 0 ? strdup(cmd) : NULL);
+
+	path = get_env_var("PATH");
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+	cmd_len = strlen(cmd);
+	dir = strtok(path_copy, ":");
+
+	while (dir)
+	{
+		dir_len = strlen(dir);
+		full_path = malloc(dir_len + cmd_len + 2);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		strcpy(full_path, dir);
+		full_path[dir_len] = '/';
+		strcpy(full_path + dir_len + 1, cmd);
+
+		if (stat(full_path, &st) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
