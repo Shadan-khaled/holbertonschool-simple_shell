5b0461aad1b6349ada2d12f2ec126108e50acd82
Shadan-khaled <Shadankhaled75@gmail.com>
2025-11-13 11:10:21 +0300

    Refactor comments and format in main.c

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
+/**
+ * file: main.c
+ * Auth: Shadan Khurji <your-email@example.com>
+ * Date: 13 Nov 2025
+ * Desc: Entry point for the simple shell program.
+ */
+
 #include "shell.h"
 
 /**
  * main - Entry point for simple shell
- * @argc: argument count
+ * @argc: argument count (unused)
  * @argv: argument vector
  *
- * Return: exit status
+ * Description: starts the shell loop by passing argv[0]
+ * to display program name in error messages.
+ *
+ * Return: exit status from shell_loop
  */
 int main(int argc, char **argv)
 {
-	(void)argc;
+    (void)argc;
 
-	return (shell_loop(argv[0]));
+    return (shell_loop(argv[0]));
 }
